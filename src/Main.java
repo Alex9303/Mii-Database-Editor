f840255129784f5a1a609bf7ca35f0a36cd6e820
Alex9303 <alexander.figart@gmail.com>
2025-02-16 09:35:57 -0500

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Simple GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("Hello, World!");
+        frame.add(label);
+
+        frame.setVisible(true);
     }
 }
